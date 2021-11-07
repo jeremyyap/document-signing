49d080962ad3f3266eca5879f341f4e85f2f3212
Jeremy Yap <jeremy.yapjl@gmail.com>
2021-11-08 01:03:52 +0800

    Add a beautiful home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './App.css';
 
 export default function App() {
   return (
     <div>
-      <h1>Bookkeeper!</h1>
+      <h1><Link to="/login">Login</Link></h1>
     </div>
   );
 }
