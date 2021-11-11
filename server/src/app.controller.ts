79fcfbac2d6fda3f82c8e4da4fe01095705fec82
Jeremy Yap <jeremy.yapjl@gmail.com>
2021-11-11 14:36:21 +0800

    Add documents routes

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,6 +1,7 @@
 import {
   Controller,
   Get,
+  Param,
   Post,
   Request,
   UseGuards,
@@ -8,12 +9,17 @@ import {
 import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
+import { DocumentsService } from './documents/documents.service';
+import { UsersService } from './users/users.service';
+import { Document } from './documents/document.entity';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private readonly authService: AuthService,
+    private readonly documentsService: DocumentsService,
+    private readonly usersService: UsersService,
   ) {}
 
   @Get()
@@ -25,5 +31,25 @@ export class AppController {
   @Post('auth/login')
   async login(@Request() req) {
     return this.authService.login(req.user);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('documents')
+  async getDocuments(@Request() req) {
+    const user = await this.usersService.findOne(req.user.id);
+    return this.documentsService.findByIDs(user.assigned_documents.concat(user.owned_documents));
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('documents/:id')
+  async getDocument(@Param('id') id: string) {
+    return this.documentsService.findOne(id);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post('documents/new')
+  async createDocument() {
+    const document = new Document();
+    return this.documentsService.save(document);
   }
 }
