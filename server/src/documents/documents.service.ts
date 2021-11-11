001679d48326e9e9fe6cd4cd9f3809d36850ab7d
Jeremy Yap <jeremy.yapjl@gmail.com>
2021-11-11 10:19:48 +0800

    Add documents service methods

--- server/src/documents/documents.service.ts
+++ server/src/documents/documents.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Document } from './document.entity';
 
 @Injectable()
-export class DocumentsService {}
+export class DocumentsService {
+  constructor(
+    @InjectRepository(Document)
+    private documentsRepository: Repository<Document>,
+  ) {}
+
+  async findByIDs(ids: Array<String>): Promise<Array<Document>> {
+    return this.documentsRepository.findByIds(ids);
+  }
+
+  async findOne(id: string): Promise<Document> {
+    return this.documentsRepository.findOne(id);
+  }
+
+  async save(document: Document): Promise<Document> {
+    return this.documentsRepository.save(document);
+  }
+}
